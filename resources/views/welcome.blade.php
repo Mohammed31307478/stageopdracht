afde0eb6618225731c07d1f1b30479de2f20feb8
Student Mootje Oudeh <moudeh1789@student.landstede.nl>
2022-07-04 14:28:47 +0200

    finished website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,6 @@
 </div>
 
 
-
 <div class="onderwerp"><h1>Maak je items</h1></div>
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -39,36 +38,37 @@
     <label class="label1"><h4>Naam voor je items </h4></label>
     <input placeholder="naam" name="name" type="text">
 
-    <label  class="label1" for="fileinput"><h4> voeg een foto toe </h4></label>
-    <input type="file"  name="image">
+    <label class="label1" for="fileinput"><h4> voeg een foto toe </h4></label>
+    <input type="file" name="image">
 
-    <label class="label1"> <h4>opmerking toevoegen </h4></label>
+    <label class="label1"><h4>opmerking toevoegen </h4></label>
     <input placeholder="opmerking" name="informatie" type="text">
     <input type="submit" placeholder="klaar">
 </form>
 
 <div class="all-items">
-<table>
-    <thead >
-        <tr >
+    <table>
+        <thead>
+        <tr>
             <th>ID</th>
-            <th  >Name</th>
+            <th>Name</th>
             <th>Image</th>
             <th>Information</th>
         </tr>
-    </thead>
-    <tbody>
-    @foreach ($todolists as $todolist)
-        <tr>
-            <td>{{$todolist->id}}</td>
-            <td>{{$todolist->name}}</td>
-            <td><img src="/{{$todolist->image}}" ></td>
-            <td>{{$todolist->informatie}}<a class="button" href="{{route('todo.edit', $todolist->id)}}">update</a></td>
-        </tr>
-    @endforeach
+        </thead>
+        <tbody>
+        @foreach ($todolists as $todolist)
+            <tr>
+                <td>{{$todolist->id}}</td>
+                <td>{{$todolist->name}}</td>
+                <td><img src="/{{$todolist->image}}"></td>
+                <td>{{$todolist->informatie}}<a class="button" href="{{route('todo.edit', $todolist->id)}}">update</a>
+                </td>
+            </tr>
+        @endforeach
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
 </div>
 
 {{--VOLTOOID FORMULEER--}}
@@ -89,18 +89,15 @@
                 <H1>voltooid</H1>
                 }
                 @else{
-                    <h1>onvoltooid</h1>
-                    }
+                <h1>onvoltooid</h1>
+                }
                 @endif
 
                 @endforeach</option>
     </select>
     <input type="checkbox" name="voltooid">
-    <input  type="submit">
+    <input type="submit">
 </form>
-
-
-
 
 
 {{--VERWIJDER IETEMS --}}
@@ -119,36 +116,34 @@
 </form>
 
 
-
-
 <div class="titelovermij">
     <a><h2>Hallo, Hier vind je persoonlijk informatie over mij</h2></a>
 </div>
-
 
 
 <section id="section2" class="overmij">
 
     <div class="overmijtekst">
         <h1>dromen</h1>
-            <p class="text"> mijn dromen zijn ten eerste dat mijn ouders trots op mij zijn. Dat is het belangrijkste. Ten tweede
-                wil
-                ik met een BMW 18 roadster rijden en een groot huis kopen en rijk leven. Zo kan ik later met
-                tijd rusten want je leeft maar 1 keer. Ik wil dus niet mijn hele leven werken.
-                Als laatste wil ik met een vrouw trouwen en kinderen krijgen. Zo kan ik gezellig leven met mijn
-                nieuwe familie.
-                Dat is alles wat ik wil.
-            </p>
+        <p class="text"> mijn dromen zijn ten eerste dat mijn ouders trots op mij zijn. Dat is het belangrijkste. Ten
+            tweede
+            wil
+            ik met een BMW 18 roadster rijden en een groot huis kopen en rijk leven. Zo kan ik later met
+            tijd rusten want je leeft maar 1 keer. Ik wil dus niet mijn hele leven werken.
+            Als laatste wil ik met een vrouw trouwen en kinderen krijgen. Zo kan ik gezellig leven met mijn
+            nieuwe familie.
+            Dat is alles wat ik wil.
+        </p>
     </div>
 
 
     <div class="overmijtekst">
         <h1> overmij </h1>
         <p class="text">
-            ik ben Mohammed. ik woon in Hoogeveen.Ik ben 18 jaar oud en ik kom uit Syrie. Ik woon nu vier
-                jaar in Nederland.
-                Ik zit op MBO landstede in Zwolle en volg de opleiding tot Software Developer. Mijn hobby's zijn
-                zwemmen, voetballen .
+            ik ben Mohammed. ik woon in Hoogeveen.Ik ben 18 jaar oud en ik kom uit Syrie. Ik woon nu vijf
+            jaar in Nederland.
+            Ik zit op MBO landstede in Zwolle en volg de opleiding tot Software Developer. Mijn hobby's zijn
+            zwemmen en voetballen.
         </p>
     </div>
 
@@ -157,10 +152,10 @@
         <h1>doel</h1>
         <p class="text">
             Het doel van deze opleiding (software developer) is dat ik later zelf websites kan maken en zelf
-                programmeren en daardoor kan ik geld verdienen en mijn dromen bereiken. Het doel van deze website is
-                het visualiseren van mijn schoolopdrachten. Ik ga hier de komende jaren laten zien wat ik ga leren
-                en heb geleerd. Ik kan hierdoor bij het zoeken van een stageplek laten zien waar ik sta. Als jij,
-                als gast, nog een vraag hebt, stel me die dan gerust!
+            programmeren en daardoor kan ik geld verdienen en mijn dromen bereiken. Het doel van deze website is
+            dat ik mijn kennis kan laten zien . Ik hoop dat de website voldoet aan jullie eisen.
+            En ik hoop dat ik uitgenodigd wordt voor kennis maak gesprek. Als jij,
+            als gast, nog een vraag hebt, stel me die dan gerust!
         </p>
     </div>
 </section>
